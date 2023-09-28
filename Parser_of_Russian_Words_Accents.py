c0e9c6e4e471b7a7e6a777506e3d9693d581a1cc
Aleksei_Zakharov <zakharov.av@inbox.ru>
2023-09-28 14:31:48 +0400

    Modernized version (3rd version)

--- Parser_of_Russian_Words_Accents.py
+++ Parser_of_Russian_Words_Accents.py
@@ -17,7 +17,7 @@ counter_of_errors: int = 0
 
 for word in list_of_words:
     try:
-        req = requests.get(f'http://gramota.ru/slovari/dic?word={word}*&all=x&help=1')
+        req = requests.get(f'https://где-ударение.рф/в-слове-{word}/')
 
     except:
         result_dict[word] = 'Error'
@@ -28,13 +28,12 @@ for word in list_of_words:
         try:
             bs = BeautifulSoup(req.text, 'html.parser')
 
-            b_tag = bs.find('div', {'style': 'padding-left:50px'}).find('b')#.find('span', {'class': 'accent'})
-            accent = b_tag.find('span', {'class': 'accent'}).text.upper()
-            b_tag_text = str(b_tag)
+            div_tag_text = bs.find('div', {'class': 'rule'}).text
 
-            result = b_tag_text[b_tag.find('>') : b_tag_text.find('s') - 1] + accent + b_tag_text[b_tag_text.rfind('n') + 2 : b_tag_text.rfind('<')]
+            result = div_tag_text[div_tag_text.rfind('й') + 6:]
 
-            result_dict[word] = result[3:]
+            result_dict[word] = result[:result.rfind('.')]
+
 
         except:
             result_dict[word] = 'Error'
@@ -46,4 +45,4 @@ pprint(result_dict)
 print(f'\nNumber of words is {len(list_of_words)}')
 print(f'Number of errors is {counter_of_errors}')
 print(f'Errors are {(counter_of_errors/ len(list_of_words)) * 100}%')
-print(f'Total time of program work: {time() - start} seconds')
+print(f'Total time of program work: {round(time() - start, 3)} seconds')
